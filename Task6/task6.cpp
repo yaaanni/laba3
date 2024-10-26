8545b51e0e157b11e73096788918792efd6a233c
yaaanni <rw.yana.rw@gmail.com>
2024-10-26 17:24:00 +0300

    added 6 task

--- Task6/task6.cpp
+++ Task6/task6.cpp
@@ -41,100 +41,103 @@ int program()
     int n;
     int i = 0;
     int sign = 1;
-        int sign1 = 1;
-            int sign2 = 1;
-long long int f = 1;
-long long int f1 = 1;
-long long int f2 = 1;
-int q = 0;
-int m = 1;
-double x, x1;
-int xm;
-cout << "Введите значение числа х (для синуса и косинуса - градусную меру угла) "<<endl; 
-cin >> x;
-x1 = x;
-int x2 = x;
- if (x >= 360){
-    double xl = x;
- double a = xl / 360;
-  int b = x / 360;
-  double c = a - b; x = c * 360;
-  }
-  if (x < 0 && x >= -360)
-  {
-  x = 360 + x;
-  }
-  if (x < -360){
-    double  xl = x;
-double a = xl / 360;
-//cout << a<< endl;
+    int sign1 = 1;
+    int sign2 = 1;
+    long long int f = 1;
+    long long int f1 = 1;
+    long long int f2 = 1;
+    int q = 0;
+    int m = 1;
+    double x, x1;
+    int xm;
+    std::cout << "Введите значение числа х (для синуса и косинуса - градусную меру угла) "<< std::endl; 
+    std::cin >> x;
+    while (std::cin.fail()){
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cout << "Введите значение повторно:";
+    std::cin >> x;
+}
+    int k = 0;
+    //Приведение угла от -360 до 360
+    while (x > 360) {
+        x /= 360;
+        k++;
+    }
+    while (x <= -360) {
+        x *= -360;
+        k--;
+    }
 
-  int b = x / 360;
-  //cout <<b<<endl;
+    x1 = x;
+    int x2 = x;
+    if (x >= 360){
+        double xl = x;
+        double a = xl / 360;
+        int b = x / 360;
+        double c = a - b; x = c * 360;
+    }
+    if (x < 0 && x >= -360){
+        x = 360 + x;
+    }
+    if (x < -360){
+        double  xl = x;
+        double a = xl / 360;
+        int b = x / 360;
+        double c = a - b; 
+        x = 360 + (c * 360);
 
-  double c = a - b; 
-  //cout<< c<<endl;
-  x = 360 + (c * 360);
-
-  }
-//cout<<x<<endl;
-xm = x;
-//cout <<"xm = "<< xm <<endl;
- if (x > 180)
- x = x - 180; 
- //cout <<x<<endl;
-if(x1<0) 
- x1 = x1*(-1);
-x = x * 3.141592 / 180;
-double d = x;
-double sin1 = 0;
-double sin2 = 1;
-double eps = 0.000001;
+}
+    xm = x;
+    if (x > 180)
+        x = x - 180; 
+    if(x1<0) 
+        x1 = x1*(-1);
+        x = x * 3.141592 / 180;
+    double d = x;
+    double sin1 = 0;
+    double sin2 = 1;
+    double eps = 0.000001;
 
 while (abs(sin1-sin2) > eps) {
     sin2 = sin1;
     sin1 = sin1 + sign * (d/f);
-    //cout <<" znach sin = " << sin1 << endl;
     d = d*x*x;
     f = f*(m+1)*(m+2);
     m = m+2;
     sign = - sign;
     i++;
 }
-//cout << "Синус угла " << x2 << " равен " << sin1 << std::endl;
 double d1 = 1;
 double cos1 = 0;
 double cos2 = 1;
 while (abs(cos1-cos2) > eps) {
     cos2 = cos1;
     cos1 = cos1 + sign1 * (d1/f1);
-    //cout<<" znach cos = " << cos1 << endl;
     d1 = d1*x*x;
     f1 = f1*(q+1)*(q+2);
     q = q+2;
     sign1 = - sign1;
     i++;
 }
-//cout<< "Косинус угла " << x2 << " равен " << cos1 << std::endl;
 double ln_x = natural_log(x1);
- //std::cout << "Натуральный логарифм числа " << x1 << " равен " << ln_x << std::endl;
-if (xm >= 0 & xm < 90)
-{if (cos1 < 0)
-cos1 = cos1 * (-1);
-if (sin1 < 0)
-sin1 = sin1 * (-1);
+if (xm >= 0 & xm < 90){
+    if (cos1 < 0)
+        cos1 = cos1 * (-1);
+    if (sin1 < 0)
+        sin1 = sin1 * (-1);
 }
-else if (xm >= 90 & xm <= 180)
-{if (cos1 > 0)
-cos1 = cos1 * (-1);
-if (sin1 < 0)
-sin1 = sin1 * (-1);
+else if (xm >= 90 & xm <= 180){
+    if (cos1 > 0)
+        cos1 = cos1 * (-1);
+    if (sin1 < 0)
+        sin1 = sin1 * (-1);
 }
-else if (xm > 180 & xm <= 270)
-{if (cos1 > 0)
-    cos1 = cos1 * (-1);
+else if (xm > 180 & xm <= 270){
+    if (cos1 > 0)
+        cos1 = cos1 * (-1);
     if (sin1 > 0)
-sin1 = sin1 * (-1);
+        sin1 = sin1 * (-1);
 }
 else if (xm > 270 & xm < 360){
 if (cos1 < 0)
@@ -142,16 +145,16 @@ cos1 = cos1 * (-1);
 if (sin1 > 0)
 sin1 = sin1 * -(1);
 }
-cout << "Синус угла " << x2 << " равен " << sin1 << std::endl;
-cout<< "Косинус угла " << x2 << " равен " << cos1 << std::endl;
-std::cout << "Натуральный логарифм числа " << x1 << " равен " << ln_x << std::endl;
+std::cout << "Синус угла равен " << sin1 << std::endl;
+std::cout<< "Косинус угла равен " << cos1 << std::endl;
+std::cout << "Натуральный логарифм числа равен " << ln_x << std::endl;
 
 if (sin1 < cos1 & sin1 < ln_x)
-cout<<"Наименьшее значение из трех величин: sin(x), cos(x) и ln(x) - это значение sin(x), = "<<sin1<<endl;
+std::cout<<"Наименьшее значение из трех величин: sin(x), cos(x) и ln(x) - это значение sin(x), = "<<sin1<<std::endl;
 if (cos1 < sin1 & cos1 < ln_x)
-cout<<"Наименьшее значение из трех величин: sin(x), cos(x) и ln(x) - это значение cos(x), = "<<cos1<<endl;
+std::cout<<"Наименьшее значение из трех величин: sin(x), cos(x) и ln(x) - это значение cos(x), = "<<cos1<<std::endl;
 if (ln_x < cos1 & ln_x < sin1)
-cout<<"Наименьшее значение из трех величин: sin(x), cos(x) и ln(x) - это значение ln(x), = "<<ln_x<<endl;
+std::cout<<"Наименьшее значение из трех величин: sin(x), cos(x) и ln(x) - это значение ln(x), = "<<ln_x<<std::endl;
 return sin1;
 }
 
